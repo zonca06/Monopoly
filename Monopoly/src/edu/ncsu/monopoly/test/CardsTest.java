9129e1d532212a34ad36eea4a1cac9ff87b80954
Martin Casanova <Martincasanova933@gmail.com>
2017-04-16 11:25:37 -0300

    Se agregan comentarios sobre la clase CardsTest 0,5h

--- Monopoly/src/edu/ncsu/monopoly/test/CardsTest.java
+++ Monopoly/src/edu/ncsu/monopoly/test/CardsTest.java
@@ -13,17 +13,22 @@ public class CardsTest extends TestCase {
     
     GameMaster gameMaster;
 
+    // En este metodo prepara el tablero y le agrega cartas que pueden salir.
     protected void setUp() {
         gameMaster = GameMaster.instance();
         gameMaster.setGameBoard(new GameBoardCCGainMoney());
         gameMaster.setNumberOfPlayers(1);
         gameMaster.reset();
         gameMaster.setGUI(new MockGUI());
+        //cada uno de los metodos de debajo agrega una carta al monton correspondiente
+        //ccCard equivale al casillero ComunityChest, Existen dos en el tablero
         ccCard = new MoneyCard("Get 50 dollars", 50, Card.TYPE_CC);
+        //chanceCard equivale al casillero Chance, existen dos en el tablero
         chanceCard = new MoneyCard("Lose 50 dollars", -50, Card.TYPE_CHANCE);
         gameMaster.getGameBoard().addCard(ccCard);
     }
     
+    //En este metodo sobre la cartas que se agregaron previamente va sacando cartas y compara con las que se agregaron
     public void testCardType() {
         Card card = gameMaster.drawCCCard();
         assertEquals(Card.TYPE_CC, ccCard.getCardType());
