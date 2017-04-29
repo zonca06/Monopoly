34792c484e227546ff255a0b786b5cb760e431c7
Alberto <Alberto@Alberto>
2017-04-29 12:17:51 -0300

    se arregla persistencia, archivo serializado desde jar 2hs

--- Monopoly/src/edu/ncsu/monopoly/Serializar.java
+++ Monopoly/src/edu/ncsu/monopoly/Serializar.java
@@ -1,8 +1,10 @@
 package edu.ncsu.monopoly;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -31,16 +33,16 @@ public class Serializar {
            
             
             
-            java.net.URL url = getClass().getResource("datosSerializados.ser");
-            System.out.println(url.getPath());
-            FileOutputStream fileOut = new FileOutputStream(url.getPath()); 
+    //        java.net.URL url = getClass().getResource("datosSerializados.ser");
+      //      System.out.println(url.getPath());
+         //   FileOutputStream fileOut = new FileOutputStream(url.getPath()); 
 
 
         // archivo de propiedades desde donde está el jar
         
-       // File jarPath=new File(GameMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-      //  String rutaArchivoSerializado=jarPath.getParentFile().getAbsolutePath();
-      //  FileOutputStream fileOut = new FileOutputStream(rutaArchivoSerializado+"/datosSerializados.ser");
+        File jarPath=new File(GameMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        String rutaArchivoSerializado=jarPath.getParentFile().getAbsolutePath();
+        FileOutputStream fileOut = new FileOutputStream(rutaArchivoSerializado+"/datosSerializados.ser");
 			
 	         
 	         
@@ -59,23 +61,33 @@ public class Serializar {
 		 try {
 			 
 			   //archivo local
-			 java.net.URL url = getClass().getResource("datosSerializados.ser");
-	            System.out.println(url.getPath());
-			 
-			 
-	            ClassLoader loader = Thread.currentThread().getContextClassLoader();
-	            FileInputStream fileIn = new FileInputStream(url.getPath());
-
+//			 java.net.URL url = getClass().getResource("datosSerializados.ser");
+//	            System.out.println(url.getPath());
+//			 
+//			 
+//	            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+//	            FileInputStream fileIn = new FileInputStream(url.getPath());
+//	            String urlArchivo=url.getPath();
 
 
 	        // archivo de propiedades desde donde está el jar
 	        
-	      // File jarPath=new File(GameMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-	     //  String rutaArchivoSerializado=jarPath.getParentFile().getAbsolutePath();
-	    //   FileInputStream fileIn = new FileInputStream(rutaArchivoSerializado+"/datosSerializados.ser");
-			 
-			 
-			 
+	       File jarPath=new File(GameMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+	       String rutaArchivoSerializado=jarPath.getParentFile().getAbsolutePath();
+	       FileInputStream fileIn = new FileInputStream(rutaArchivoSerializado+"/datosSerializados.ser");
+	          String urlArchivo=rutaArchivoSerializado+"/datosSerializados.ser";	 
+		 
+		
+	            
+	            
+	            
+	          
+	            
+	          //verifico si hay algo en el archivo 
+	            BufferedReader br = new BufferedReader(new FileReader(urlArchivo));     
+	            if (br.readLine() != null) {
+	                
+	            
 			 
 	         
 	         ObjectInputStream in = new ObjectInputStream(fileIn);
@@ -84,13 +96,23 @@ public class Serializar {
 	         fileIn.close();
 	         
           return listaDePerfilesPersistidos;
+	      }else{
+	    	  //si no hay nada retorno una lista vacía
+	    	  return new ArrayList<Perfil>();
+	      }
+	            
+	            
+	            
 	      }catch(IOException i) {
 	         i.printStackTrace();
-	         return null;
+	         return new ArrayList<Perfil>();
 	      }catch(ClassNotFoundException c) {
 	         System.out.println("No se encontraron los usuarios");
 	         c.printStackTrace();
-	         return null ;
+	         return new ArrayList<Perfil>();
+	      }catch(Exception ex){
+	    	  
+	    	  return new ArrayList<Perfil>();
 	      }
 
 		
